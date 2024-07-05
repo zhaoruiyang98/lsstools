85b89febde90df13de906a731865100e2635fd90
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2024-07-05 18:24:02 +0100

    update slurm test

--- tests/dev/test_slurm.py
+++ tests/dev/test_slurm.py
@@ -1,11 +1,11 @@
 from lsstools.mpi import COMM_WORLD
-from lsstools.ex.slurm import Scheduler, TaskManager
+from lsstools.ex.slurm import Scheduler, TaskManager, optional
 
 
-def main(x, y, z="X"):
+def main(x, y, z="X", random_fail=optional(bool)):
     from random import random
 
-    if random() < 0.1:
+    if random_fail and random() < 0.1:
         # 10% random failure
         raise ValueError
     print(f"{x} {y} {z}")
@@ -18,9 +18,9 @@ if __name__ == "__main__":
         cpus_per_task=4,
         time_limit="00:01:00",
     )
-    tm = TaskManager(func=main, script_path="jobs/test.sh", scheduler=scheduler, __file__=__file__)
+    tm = TaskManager(func=main, script_path="jobs/test.sh", scheduler=scheduler)
     for x, y in zip([1, 2, 3], ["a", "b", "c"]):
         for z in ["X", "Y"]:
-            tm(x=x, y=y, z=z)
+            tm(x=x, y=y, z=z, random_fail=True)
     tm.run()
     # tm.retry_failed()
